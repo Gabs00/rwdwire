0aeffae648cec927ed8cd838c3ae32d6d2439b5f
Gabs00 <gewen87@gmail.com>
2014-10-25 13:24:06 -0400

    added angular folder for angular version

--- server/app.js
+++ server/app.js
@@ -3,13 +3,16 @@ var app = require('express')();
 app.get('/*', function(req,res,next){
   console.log(req.url);
 
-  if(req.url === '/'){
+  if(req.url === '/' || req.url === '/angular'){
     next();
   } else {
     res.sendFile(req.url, {root:'./public'});
   }
 });
 
+app.get('/angular', function(req, res){
+  res.sendFile('index.html', {root:'./public/angular'});
+});
 app.get('/', function(req, res){
   res.sendFile('index.html', {root:'./'});
 });
